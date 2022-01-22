06598c6ec80cda99db93f174b1965acb9cae328d
Andu15 <estefania_8_3@hotmail.com>
2022-01-21 20:23:52 -0500

    feat: add third home view structure

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import LogoApp from '../assets/images/logoHomebanking.png';
+
 const Logo = () => {
   return (
     <div>
-      
+      <figure>
+        <img className='logo' src={LogoApp} alt="logo Banco Saint Patrick" />
+        <figcaption>
+          <span className='text-bank'>BANCO</span>
+          <span className='text-name'>SAINT PATRICK</span>
+        </figcaption>
+      </figure>
     </div>
   )
 }
--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { InformativeScreens } from "../components/InformativeScreens.jsx";
 
+import Logo from '../components/Logo';
+
 const Start = () => {
   return (
     <>
-      <InformativeScreens />
+      {/* <InformativeScreens /> */}
+      <Logo />
     </>
   );
 };
